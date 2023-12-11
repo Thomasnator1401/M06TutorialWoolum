c13e98335cb113fe92a19b1aa29e67f17274a13f
Thomasnator1401 <145989955+Thomasnator1401@users.noreply.github.com>
2023-12-10 21:29:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,58 @@
-// Connecting to MongoDB using mongoose
+// Require the modules
+const express = require('express');
 const mongoose = require('mongoose');
-const connectionString = 'mongodb://localhost:27017/mydb';
 
-mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((result) => console.log('connected to db'))
-  .catch((err) => console.log(err));
+// Create the app
+const app = express();
 
-// Creating a schema and a model for a blog post
-const Schema = mongoose.Schema;
-const blogSchema = new Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  snippet: {
-    type: String,
-    required: true
-  },
-  body: {
-    type: String,
-    required: true
-  }
-}, { timestamps: true });
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/node-tutorial', {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error(err));
 
-const Blog = mongoose.model('Blog', blogSchema);
-
-// Saving a new blog post to the database
-const blog = new Blog({
-  title: 'My first blog post',
-  snippet: 'This is a short summary of my blog post',
-  body: 'This is the full content of my blog post'
+// Define a schema for the blog posts
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+  date: Date
 });
 
-blog.save()
-  .then((result) => console.log(result))
-  .catch((err) => console.log(err));
-// Retrieving all blog posts from the database
-Blog.find()
-  .then((result) => console.log(result))
-  .catch((err) => console.log(err));
-// Retrieving a specific blog post by its ID  
+// Create a model for the blog posts
+const Post = mongoose.model('Post', postSchema);
+
+// Create a route for the home page
+app.get('/', (req, res) => {
+  // Find all the blog posts from the database
+  Post.find()
+    .then(posts => {
+      // Render the home page with the posts
+      res.render('index', {posts: posts});
+    })
+    .catch(err => console.error(err));
+});
+
+// Create a route for adding a new post
+app.post('/new', (req, res) => {
+  // Get the data from the request body
+  const title = req.body.title;
+  const content = req.body.content;
+  const date = new Date();
+
+  // Create a new post object
+  const post = new Post({
+    title: title,
+    content: content,
+    date: date
+  });
+
+  // Save the post to the database
+  post.save()
+    .then(() => {
+      // Redirect to the home page
+      res.redirect('/');
+    })
+    .catch(err => console.error(err));
+});
+
+// Start the server
+app.listen(3000, () => console.log('Server is running on port 3000'));
